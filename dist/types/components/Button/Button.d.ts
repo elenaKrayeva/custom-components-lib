c50e03d6406a52486a79fd5bd50346e4f73c8ea1
Elena.Krayeva <alenakrayeva@gmail.com>
2025-08-29 16:32:48 +0300

    chore: stop tracking dist/

--- dist/types/components/Button/Button.d.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    label: string;
-}
-export declare const Button: React.FC<ButtonProps>;
